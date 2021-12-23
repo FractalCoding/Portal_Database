a9e724f82ea941039889128152c7fd427572856e
Jim O'Brien <git@jimmed.net>
2021-12-23 13:01:49 +0000

    add scraper logic

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Node.js development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = with pkgs;
+          let nodejs = nodejs-16_x;
+          in mkShell {
+            nativeBuildInputs = [
+              bashInteractive
+              git
+              nodejs
+              nixfmt
+            ];
+          };
+      });
+}
